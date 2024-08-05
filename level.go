11c38dcdb6b76d429da99852a04d77135322b071
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-08-06 07:37:10 +1000

    add level type

---
+++ level.go
@@ -0,0 +1,4 @@
+package pocketlog
+
+// Level represents the available level of logging
+type Level byte
--- logger.go
+++ logger.go
@@ -1,4 +1,5 @@
 package pocketlog
 
+// Logger is used to log certain information
 type Logger struct {
 }
