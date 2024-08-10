98d3cc4dcb7a55bd3663c2479c2a1261152e94e1
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-08-10 21:37:00 +1000

    add logger obj

--- logger.go
+++ logger.go
@@ -2,4 +2,12 @@ package pocketlog
 
 // Logger is used to log certain information
 type Logger struct {
+	threshold Level
+}
+
+// New returns you a logger, ready to log at the required threshold.
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
 }
